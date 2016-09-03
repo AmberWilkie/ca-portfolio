0b4eedf0ef99a608e2028d26c47596c3e09c02a5
Amber <amber@amberwilkie.com>
2016-09-03 08:54:02 +0200

    Add in deployment options in config

--- config.rb
+++ config.rb
@@ -69,3 +69,9 @@ configure :build do
   # Minify Javascript on build
   # activate :minify_javascript
 end
+
+activate:deploy do |deploy|
+  deploy.method = :git
+  deploy.branch = ’master’
+  deploy.build_before = true
+end
