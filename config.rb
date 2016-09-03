d1cdcdefc19c747598fe98ea57ba2b00a6eada48
Amber <amber@amberwilkie.com>
2016-09-03 09:15:56 +0200

    Remove title from blog permalink

--- config.rb
+++ config.rb
@@ -24,7 +24,7 @@ activate :blog do |blog|
   # This will add a prefix to all links, template references and source paths
   # blog.prefix = "blog"
 
-  blog.permalink = "/articles/{year}/{month}/{day}/{title}.html"
+  blog.permalink = "/articles/{year}-{month}-{day}.html"
   # Matcher for blog source files
   blog.sources = "/articles/{year}-{month}-{day}.html"
   # blog.taglink = "tags/{tag}.html"
