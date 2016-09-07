2867ecb896cf453fa4db0c81f9e20ae741f91f6b
Amber <amber@amberwilkie.com>
2016-09-07 10:25:46 +0200

    Add deploy bits in config file

--- config.rb
+++ config.rb
@@ -71,8 +71,8 @@ configure :build do
   # activate :minify_javascript
 end
 
-# activate:deploy do |deploy|
-#   # deploy.method = :git
-#   # deploy.branch = ’master’
-#   # deploy.build_before = true
-# end
+activate :deploy do |deploy|
+  deploy.deploy_method = :git
+  deploy.branch = 'master'
+  deploy.build_before = true
+end
