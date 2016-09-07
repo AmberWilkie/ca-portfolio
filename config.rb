846830d264b4aac5bc675552802898e4e8cceebf
Amber <amber@amberwilkie.com>
2016-09-07 10:40:01 +0200

    Require build_cleaner, deploy working now

--- config.rb
+++ config.rb
@@ -1,40 +1,14 @@
-###
-# Page options, layouts, aliases and proxies
-###
+require 'extensions/build_cleaner'
 
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-###
-# Helpers
-###
-
 activate :blog do |blog|
-  # This will add a prefix to all links, template references and source paths
-  # blog.prefix = "blog"
-
   blog.permalink = "/articles/{year}-{month}-{day}.html"
   # Matcher for blog source files
   blog.sources = "/articles/{year}-{month}-{day}.html"
   # blog.taglink = "tags/{tag}.html"
   blog.layout = "./layouts/blog_layout"
-  # blog.summary_separator = /(READMORE)/
-  # blog.summary_length = 250
-  # blog.year_link = "{year}.html"
-  # blog.month_link = "{year}/{month}.html"
-  # blog.day_link = "{year}/{month}/{day}.html"
-  # blog.default_extension = ".markdown"
 
   blog.tag_template = "blog/tag.html"
   blog.calendar_template = "blog/calendar.html"
@@ -64,15 +38,12 @@ end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :relative_assets
+  activate :build_cleaner
 end
 
 activate :deploy do |deploy|
   deploy.deploy_method = :git
-  deploy.branch = 'master'
+  # deploy.branch = 'master'
   deploy.build_before = true
 end
